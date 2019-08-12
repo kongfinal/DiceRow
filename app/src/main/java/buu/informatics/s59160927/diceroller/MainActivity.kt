f92627f748aa6cdfe752a0bbf7afa0226cae8411
59160927 <59160927@go.buu.ac.th>
2019-08-11 23:30:37 -0700

    Task: Find views efficiently

--- app/src/main/java/buu/informatics/s59160927/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160927/diceroller/MainActivity.kt
@@ -3,15 +3,18 @@ package buu.informatics.s59160927.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -19,21 +22,27 @@ class MainActivity : AppCompatActivity() {
         val resetButton: Button = findViewById(R.id.reset_button)
         resetButton.setOnClickListener { resetDice() }
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 
     private fun resetDice() {
         val randomInt = 0
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
